82e41abbdbf1f56823caac3c8f8a814b1103fef1
org-odajs <82090162+org-odajs@users.noreply.github.com>
2022-08-31 10:37:12 +0300

    Add Odajs Framework To The List

--- library-list.js
+++ library-list.js
@@ -3,6 +3,7 @@
   var list = [
     { type: 'todo', id: 'aurelia', label: 'DBMON Aurelia', url: '#' },
     { type: 'naive', id: 'ember', url: './ember', label: 'DBMON Ember' },
+    { type: 'odajs', id: 'odajs', url: './odajs', label: 'ODAJS'},
     { type: 'naive', id: 'angular', url: './angular', label: 'DBMON Angular' },
     { type: 'naive', id: 'angular-light', url: './angular-light', label: 'DBMON Angular Light' },
     { type: 'naive', id: 'angular2', url: './angular2', label: 'DBMON Angular 2.0 Alpha' },
